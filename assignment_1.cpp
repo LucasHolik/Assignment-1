b8a0572e7513a04c29582cd464db9281320346ed
Lucas Holik <lucas.holik88@gmail.com>
2025-01-27 15:56:43 +0000

    First working program

--- assignment_1.cpp
+++ assignment_1.cpp
@@ -1,3 +1,6 @@
+//Assignment 1/assignment_1.cpp
+
+
 #include <iostream>
 #include <cmath>
 
@@ -11,11 +14,11 @@ float transition_energy(int n1, int n2, int Z) {
 
 
 
-int main() {
-
-std::cout << "This programs calculates the energy of the transition of an electron in an atom." << std::endl;
+int main() 
+{
+std::cout<<"This programs calculates the energy of the transition of an electron in an atom with atomic number Z." << std::endl;
 int n1, n2, Z;
-std::cout << "Please enter the initial energy level of the electron: ";
+std::cout<<"Please enter the initial energy level of the electron: ";
 std::cin >> n1;
 std::cout << "Please enter the final energy level of the electron: ";
 std::cin >> n2;
@@ -26,4 +29,10 @@ float energy = transition_energy(n1, n2, Z);
 
 std::cout << "The energy of the transition is: " << energy << " eV" << std::endl;
 
+std::cout<<"Would you like to calculate another transition energy? (y/n): ";
+char response;
+std::cin>>response;
+
+if(response == 'y') 
+    main();
 }
