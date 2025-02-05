eac9b72b132dea04d9042fb33312c525eb19c5af
Lucas Holik <lucas.holik88@gmail.com>
2025-02-05 11:18:05 +0000

    Pretty happy with everything, going to check on lab computer and then submit

--- assignment_1.cpp
+++ assignment_1.cpp
@@ -81,16 +81,16 @@ std::tuple<int,int> get_energy_levels()
     while(!valid_input)
     {
         try
-        {
+        {   //Get first energy level.
             n_i = get_positive_integer("Please enter the initial energy level of the electron: ");
-
+            //n_i must be 2 or more.
             if(n_i == 1)
             {
                 throw std::invalid_argument("The initial energy level must be greater than or equal to 2.");
             }
-
+            //Get second energy level
             n_j = get_positive_integer("Please enter the final energy level of the electron: ");
-
+            // Check that energy levels are valid.
             if(n_i > n_j)
             {
                 valid_input = true;
@@ -100,27 +100,29 @@ std::tuple<int,int> get_energy_levels()
                 throw std::invalid_argument("The initial energy level must be greater than the final energy level.");
             }
         }
+        //Catch any exceptions.
         catch(const std::invalid_argument &e)
         {
             std::cerr<<e.what()<<'\n';
         }
         
     }
+    //Pack n_i and n_j into a tuple and return.
     std::tuple<int,int> energy_levels = std::make_tuple(n_i, n_j);
     return energy_levels;
 }
 
 std::string get_energy_unit()
-{   
+{   //
     std::string unit;
     bool valid_input{false};
 
     while(!valid_input)
-    {
+    {   //Gets the user to input 
         std::cout<<"Would you like the energy to be displayed in joules or electron-volts? Type 'J' or 'eV': ";
         std::cin >> unit;
-
-        if(unit == "J" || unit == "eV")
+        //Cover all cases, upper and lower case.
+        if(unit == "J" || unit == "j" || unit == "eV" || unit == "ev" || unit == "Ev" || unit == "EV")
         {
             valid_input = true;
         }
@@ -129,25 +131,39 @@ std::string get_energy_unit()
             std::cout<<"Please enter a valid response."<<std::endl;
         }
     }
+    //Redefine unit in such that get_transition_energy can use it.
+    if(unit == "j" || unit == "J")
+    {
+        unit = "J";
+    }
+    else
+    {
+        unit = "eV";
+    }
 
     return unit;
 }
 
 int main()
-{
+{   
+    //Gives the user some information about the program and electron transitions.
     std::cout<<"This programs calculates the energy of an emitted photon due to an electron transition between energy levels in an atom of atomic number Z."<<std::endl;
-    std::cout<<"The electron's initial quantum number must be greater than its final quantum number. Quantum numbers must be positive integers, hence the minimum values for the inital and final quantum numbers are 2 and 1 respectively."<<std::endl;
+    std::cout<<"The electron's initial quantum number (n) must be greater than its final quantum number. Quantum numbers n must be positive integers, hence the minimum values for the inital and final quantum numbers are 2 and 1 respectively."<<std::endl;
 
     int n_i, n_j;
-
+    //Get the energy levels.
     std::tuple<int,int> energy_levels = get_energy_levels();
+    //Unpack the tuple_energy_levels.
     std::tie(n_i, n_j) = energy_levels;
-
+    //Get the atomic numnber.
+    int Z{get_positive_integer("Please enter the atomic number of the atom: ")}; //Get atomic number.
+    //Get the unit.
     std::string unit = get_energy_unit();
-
-    double transition_energy = get_transition_energy(n_i, n_j, 1, unit);
-
+    //Calculate the transition energy.
+    double transition_energy = get_transition_energy(n_i, n_j, Z, unit);
+    //Set the precision to 3 significant figures since the equation contains 13.6 which is to 3 significant figures.
     std::cout << std::scientific << std::setprecision(2);
+    //Display the transition energy.
     std::cout<<"The transition energy is: "<<transition_energy<<" "<<unit<<" to 3 significant figures"<<std::endl;
 
     return 0;
